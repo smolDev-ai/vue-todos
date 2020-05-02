8ab9d2eb797fa3b0ae895600e8146e8eba757dbf
Megan <megan.miller@smoldev.dev>
2020-05-02 01:12:48 -0500

    Todo Form created

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png" alt="">
-    <h1>
-      Vue Todos
-    </h1>
+    <img src="./assets/logo.png" alt />
+    <h1>Vue Todos</h1>
+    <TodoForm />
   </div>
 </template>
 
 <script>
+import TodoForm from "./components/TodoForm";
 
 export default {
-  name: 'App',
-  
-}
+  name: "App",
+  components: {
+    TodoForm
+  }
+};
 </script>
 
 <style>
---
+++ src/components/TodoForm.vue
@@ -0,0 +1,16 @@
+<template>
+  <form>
+    <input type="text" name="todo" placeholder="...todo" />
+    <button type="submit">Add Todo</button>
+    <button>Clear Completed</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "TodoForm"
+};
+</script>
+
+<style>
+</style>
